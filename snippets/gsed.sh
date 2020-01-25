6639caef4b18c5185a184ef550be2f04494f3e65
Damian Mee <spam@meedamian.com>
2020-01-25 19:12:40 +0700

    I will not apologize for art.

---
+++ snippets/gsed.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# If `gsed` is available in the system, then use it instead as the available `sed` might not be too able (MacOS)…
+SED="sed"
+if command -v gsed >/dev/null; then
+  SED=gsed
+fi
+
+
+## Usage ex:
+# ${SED} -i  -e "…"  "${file}"
