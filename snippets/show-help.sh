15eb685d413516938e2b80af87c80ab52e00da04
Damian Mee <spam@meedamian.com>
2020-06-21 19:00:19 +0800

    Update show-help

--- snippets/show-help.sh
+++ snippets/show-help.sh
@@ -3,39 +3,48 @@
 ## Handy way to quickly add help screen to a script.
 #    Best, if located on top of the file
 
+VERSION=vX.Y.Z
+NAME="$(basename "$0")"
+BLNK="$(echo "$NAME" | sed "s|.| |g")"
+
+GH_URL=https://github.com/meeDamian/useful
+
+show_version() { echo "$NAME $VERSION"; }
 show_help() {
   cat << EOF >&2
-SCRIPT-FILENAME vX.Y.Z
+$(show_version)
 
 BRIEF_ONELINE_DESCRIPTION
 
-Usage: SCRIPT-FILENAME [OPTIONAL] REQUIRED
+Usage: $NAME [OPTIONAL] REQUIRED
 
 [Where EXPLANATION_OF_ABOVE_IF_NEEDED]
 
 Options:
 
-  -h, --help, help      Show this help message
-  -L, --LIST_ALL        LIST OF FLAGS THE SCRIPT TAKES
-  -v, --version         Download to a specified dir (will be created, if doesn't exist)
+  -h, --help, help        Show this help message
+  -<L>, --<LIST_ALL>      <LIST OF FLAGS THE SCRIPT TAKES>
+  -v, --version, version  Show version, and exit
 
 Examples:
 
-  SCRIPT-FILENAME EXAMPLE-CMD                     # Explanation
-  SCRIPT-FILENAME --FLAG ANOTHER-CMD              # Another explanation
-  SCRIPT-FILENAME --WOW-IT DOES-3-THINGS          # This explanation, on the other hand is longer than a single
-                                                  #    line, and had to be wrapped to another!
+  $NAME EXAMPLE-CMD                     # Explanation
+  $NAME --FLAG ANOTHER-CMD              # Another explanation
+  $NAME --WOW-IT DOES-3-THINGS          # This explanation, on the other hand is longer than a single
+  $BLNK                                 #    line, and had to be wrapped to another!
 
-github: GITHUB_URL
+github: $GH_URL
 
 EOF
 }
 
 _usage() {
-	# If no arguments passed, show help and exit
-	if [ "$#" -le 0 ]; then
-		show_help
-		exit 1
-	fi
+	case "${1#--}" in
+	'')         show_help;    exit 1 ;;
+	-h|help)    show_help;    exit 0 ;;
+	-v|version) show_version; exit 0 ;;
+	esac
 }
-#_usage
+
+# shellcheck disable=SC2048,SC2086
+_usage $*
